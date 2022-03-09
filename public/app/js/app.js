414188c08534f43a56b2b371c8112eb967da4a74
hason2k <sonspear2000@gmail.com>
2022-03-09 11:56:05 +0700

    fix loi anh Nghia cmt lan 1

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,14 +1,15 @@
-$("#header-btnIcon").click(function() {
-  if ($(".header-content").hasClass("show")) {
-    setTimeout(function() {
-      $(".navbar-toggler-icon").removeClass("hide");
-      $(".fa-times").addClass("hide");
-    }, 100);
-  } else {
-    setTimeout(function() {
-      $(".navbar-toggler-icon").addClass("hide");
-      $(".fa-times").removeClass("hide");
-    }, 40);
-  }
+$(document).ready(function(){
+  $("#headerBtnIcon").click(function() {
+    if ($(".header-content").hasClass("show")) {
+      setTimeout(function() {
+        $(".navbar-toggler-icon").removeClass("hide");
+        $(".fa-times").addClass("hide");
+      }, 100);
+    } else {
+      setTimeout(function() {
+        $(".navbar-toggler-icon").addClass("hide");
+        $(".fa-times").removeClass("hide");
+      }, 40);
+    }
+  });
 });
-
