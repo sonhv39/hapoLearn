c421bb4a6e113709908e9f7ae2358eed29967e95
hason2k <sonspear2000@gmail.com>
2022-03-10 14:16:15 +0700

    sua loi anh Nghia cmt lan7

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $("#headerBtnIcon").click(function() {
-    if ($(".header-content").hasClass("show")) {
-      $(".navbar-toggler-icon").removeClass("hide");
-      $(".fa-times").addClass("hide");
+  $('#headerBtnIcon').click(function() {
+    if ($('.header-content').hasClass('show')) {
+      $('.navbar-toggler-icon').removeClass('hide');
+      $('.fa-times').addClass('hide');
     } else {
-      $(".navbar-toggler-icon").addClass("hide");
-      $(".fa-times").removeClass("hide");
+      $('.navbar-toggler-icon').addClass('hide');
+      $('.fa-times').removeClass('hide');
     }
   });
 });
