a01544e1cabcbc3cd19b7a4021e895bcc3c05cba
sonspear <sonspear2000@gmail.com>
2022-04-04 14:36:42 +0700

    fix convention 1

--- database/seeders/UsersCoursesSeeder.php
+++ database/seeders/UsersCoursesSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\UserCourse;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
--- database/seeders/UsersLessonsSeeder.php
+++ database/seeders/UsersLessonsSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\UserLesson;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
