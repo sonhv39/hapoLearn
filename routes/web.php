e3583ec86f83692f50445c9934648dfd0d2d3fdc
sonspear <sonspear2000@gmail.com>
2022-04-06 11:26:54 +0700

    ignore file editorconfig, modify gitignore, and modify folder Controler, Requests

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,10 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Auth::routes();
 
 Route::get('home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Auth::routes();
 
-//Route::resource('')
