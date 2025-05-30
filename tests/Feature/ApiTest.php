d6e7e163e07e8b9e85f513f9543bd4326decf266
sonhv <sonhv@rikkeisoft.com>
2025-05-30 10:01:24 +0700

    Fix PHPCS errors in test files

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -6,19 +6,19 @@ use Tests\TestCase;
 
 class ApiTest extends TestCase
 {
-    public function test_api_returns_200()
+    public function testApiReturns200()
     {
         $response = $this->get('/api/health-check');
         $response->assertStatus(200);
     }
 
-    public function test_api_returns_json()
+    public function testApiReturnsJson()
     {
         $response = $this->get('/api/health-check');
         $response->assertHeader('Content-Type', 'application/json');
     }
 
-    public function test_api_returns_404_for_invalid_endpoint()
+    public function testApiReturns404ForInvalidEndpoint()
     {
         $response = $this->get('/api/invalid-endpoint');
         $response->assertStatus(404);
--- tests/Feature/BasicPagesTest.php
+++ tests/Feature/BasicPagesTest.php
@@ -6,19 +6,19 @@ use Tests\TestCase;
 
 class BasicPagesTest extends TestCase
 {
-    public function test_home_page_loads()
+    public function testHomePageLoads()
     {
         $response = $this->get('/');
         $response->assertStatus(200);
     }
 
-    public function test_login_page_loads()
+    public function testLoginPageLoads()
     {
         $response = $this->get('/login');
         $response->assertStatus(200);
     }
 
-    public function test_register_page_loads()
+    public function testRegisterPageLoads()
     {
         $response = $this->get('/register');
         $response->assertStatus(200);
